f3b56a5b9d6c1e4f024db25cd15cfa25d3e69504
Daniel Kondrashoff <yy.indy@yandex.ru>
2025-02-03 17:45:22 +0300

    update: architecture, folder system & external dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,39 @@
 
 #include <QtCore>
 #include <iostream>
+#include <memory>
+#include <boost/asio.hpp>
+#include <spdlog/spdlog.h>
 
+/*
+class Logger 
+{
+public:
+    static std::shared_ptr<spdlog::logger> logger;
+
+    static void init()
+    {
+        logger = std::make_shared<spdlog::logger>("Server");
+    }
+
+    static std::shared_ptr<spdlog::logger>& getLogger() 
+    {
+        return logger;
+    }
+};
+*/
 int main(int argc, char** argv)
 {
-	std::cout << "hello world!" << std::endl;
+    //Logger::init();
+    
+    //Logger::getLogger()->info("Server is active!");
+
+    boost::asio::io_context ioContext;
+    unsigned short port = 8080;
+    //Server server(ioContext, port);
+    //server.startAccepting();
+
+    ioContext.run();
 
 	return 0;
 }
